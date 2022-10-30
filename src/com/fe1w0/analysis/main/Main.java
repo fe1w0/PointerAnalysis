16be38ff8a0e2807585e9cc804565b7c3ed15d24
fe1w0 <xzasliuxinrong@gmail.com>
2022-10-30 15:00:23 +0800

    Deevlop: maybe develop 0.3 version, add the Steensgaard algorithm?

--- src/com/fe1w0/analysis/main/Main.java
+++ src/com/fe1w0/analysis/main/Main.java
@@ -2,16 +2,12 @@ package com.fe1w0.analysis.main;
 
 import com.fe1w0.analysis.util.PointerAnalysis;
 
-import java.io.File;
-
-import static com.fe1w0.analysis.util.HandleOutput.SaveOutput;
-
 public class Main {
-    // Main 先不着急写，先看看他们指针分析是怎么做的
-    // Main 主要接受 两个参数
+    // Main 先不着急写，先完成指针分析
     // arg 1 : sourceDirectory, class 地址
     // arg 2 : analysisClassName 检测函数名
-    // 先输出 所有的 body 中的语句
+    // Phase1 : 输出 所有的 body 中的语句
+    // Phase2 : Pointer Analysis
 
     public static void main(String[] args) {
         String sourceDirectory = "sample";
